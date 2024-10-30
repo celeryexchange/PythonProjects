8c960308ed394e68b2f5a8d107f6364237ad856c
Martin Jakubco <70706042+mjakubco-tm@users.noreply.github.com>
2024-10-30 17:35:32 +0000

    Assign access to Key Vault and enable Log Analytics

--- StockNewsAlert/deploy-to-azure.ps1
+++ StockNewsAlert/deploy-to-azure.ps1
@@ -1,4 +1,5 @@
 # (Warning) Manual action needed for Steps 10-11 (creating identity and assigning role)
+# (Warning) If you don't have a dedicated Azure Log Analytics Workspace, please create it beforehand.
 
 # code assumes you're using Azure CLI and Powershell and are already logged in
 # az login
@@ -6,20 +7,29 @@
 # Variables
 $RESOURCE_GROUP="StockAlertApp"
 $LOCATION="westeurope"
-$APP_NAME="StockAlertApp"
 $CONTAINER_REGISTRY_NAME="stockalertappregistry"
 $CONTAINER_REGISTRY_SKU="basic"
 $DOCKER_IMAGE_NAME="stock-alert-app"
 $DOCKER_IMAGE_TAG="latest"
-$PLAN_NAME="StockAlertAppServicePlan"
-$PLAN_SKU="F1"  # F1: free 60 minutes/day; B1: basic £12/month
 $KEY_VAULT_NAME="StockAlertAppKeyVault"
-$WEBAPP_NAME="stock-alert--web-app"
 $LOGIC_APP_NAME="StockAlertAppLogicApp"
 $ACI_NAME="news-alert-app-container"
+$LOG_ANALYTICS_WORKSPACE_NAME="LogAnalyticsWorkspace"
+$LOG_ANALYTICS_RESOURCE_GROUP="LogAnalytics"
 
-# Look up this variable from the currently active subscription
+# Look up variables from the currently active subscription
 $SUBSCRIPTION_ID =$(az account show --query "id" -o tsv)
+
+$LOG_ANALYTICS_KEY = $(az monitor log-analytics workspace get-shared-keys `
+    --resource-group $LOG_ANALYTICS_RESOURCE_GROUP `
+    --workspace-name $LOG_ANALYTICS_WORKSPACE_NAME `
+    --query primarySharedKey `
+    -o tsv)
+
+$LOG_ANALYTICS_WORKSPACE_ID = $(az monitor log-analytics workspace show `
+  --resource-group $LOG_ANALYTICS_RESOURCE_GROUP `
+  --workspace-name $LOG_ANALYTICS_WORKSPACE_NAME `
+  --query customerId -o tsv)
 
 # Step 1: Create a Resource Group
 az group create --name $RESOURCE_GROUP --location $LOCATION
@@ -78,24 +88,32 @@ az keyvault secret set --vault-name $KEY_VAULT_NAME --name "TWILIO-AUTH-TOKEN" -
 # Step 6: Build a Docker image and push it to Azure Container Registry
 az acr build --registry $CONTAINER_REGISTRY_NAME --resource-group $RESOURCE_GROUP --image $DOCKER_IMAGE_NAME .
 
-# Step 7: Deploy the container with Azure Container Instances, injecting secrets from Key Vault as environment variables
 # Get secrets from Key Vault to pass into the container
 $AlphaVantageApiKey = az keyvault secret show --vault-name $KEY_VAULT_NAME --name "ALPHA-VANTAGE-API-KEY" --query "value" -o tsv
 $NewsApiKey = az keyvault secret show --vault-name $KEY_VAULT_NAME --name "NEWS-API-KEY" --query "value" -o tsv
 $TwilioAccountSID = az keyvault secret show --vault-name $KEY_VAULT_NAME --name "TWILIO-ACCOUNT-SID" --query "value" -o tsv
 $TwilioAuthToken = az keyvault secret show --vault-name $KEY_VAULT_NAME --name "TWILIO-AUTH-TOKEN" --query "value" -o tsv
 
-# Get the Docker image URL
+# Deploy the container with ACI and with a System-Assigned Managed Identity
 $acrImageUrl = "${CONTAINER_REGISTRY_NAME}.azurecr.io/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
-
-# Deploy the container with ACI
-az container create --resource-group $RESOURCE_GROUP --name $ACI_NAME --image $acrImageUrl `
+az container create `
+  --resource-group $RESOURCE_GROUP `
+  --name $ACI_NAME `
+  --image $acrImageUrl `
   --cpu 1 --memory 1.5 `
+  --assign-identity `
+  --registry-login-server "$CONTAINER_REGISTRY_NAME.azurecr.io" `
   --registry-username $(az acr credential show --name $CONTAINER_REGISTRY_NAME --query "username" -o tsv) `
   --registry-password $(az acr credential show --name $CONTAINER_REGISTRY_NAME --query "passwords[0].value" -o tsv) `
-  --registry-login-server "$CONTAINER_REGISTRY_NAME.azurecr.io" `
-  --environment-variables ALPHA_VANTAGE_API_KEY=$AlphaVantageApiKey NEWS_API_KEY=$NewsApiKey TWILIO_ACCOUNT_SID=$TwilioAccountSID TWILIO_AUTH_TOKEN=$TwilioAuthToken `
-  --restart-policy Never
+  --restart-policy Never `
+  --log-analytics-workspace $LOG_ANALYTICS_WORKSPACE_ID `
+  --log-analytics-workspace-key $LOG_ANALYTICS_KEY `
+#   --secure-environment-variables ALPHA_VANTAGE_API_KEY=$AlphaVantageApiKey NEWS_API_KEY=$NewsApiKey TWILIO_ACCOUNT_SID=$TwilioAccountSID TWILIO_AUTH_TOKEN=$TwilioAuthToken `
+
+# # Step 8: Grant ACI access to Azure Key Vault
+$ACI_PRINCIPAL_ID = $(az container show --name $ACI_NAME --resource-group $RESOURCE_GROUP --query "identity.principalId" -o tsv)
+az keyvault set-policy --name $KEY_VAULT_NAME --secret-permissions get --object-id $ACI_PRINCIPAL_ID
+
 
 # Step 8: Create a JSON file to configure triggering the Azure Container Instances daily at 8pm
 $LOGIC_APP_DEFINITION_PATH = ".\logicAppDefinition.json"
@@ -138,7 +156,8 @@ az logic workflow create `
     --resource-group $RESOURCE_GROUP `
     --name $LOGIC_APP_NAME `
     --location $LOCATION `
-    --definition @$LOGIC_APP_DEFINITION_PATH
+    --definition @$LOGIC_APP_DEFINITION_PATH `
+    --mi-system-assigned true
 
 # Step 10: Create system-assigned identity for the Logic App
 # ref: https://learn.microsoft.com/en-us/cli/azure/logic/workflow/identity?view=azure-cli-latest#az-logic-workflow-identity-assign
@@ -148,5 +167,53 @@ az logic workflow create `
 # ref: https://learn.microsoft.com/en-us/cli/azure/role/assignment?view=azure-cli-latest#az-role-assignment-create
 # Do this manually
 
-# Step 12: Remove the JSON definitions file after deployment
+
+
+# Optional: Enable log analytics for Azure Container Instances
+# Get the resource ID for Log Analytics workspace
+
+
+# # Enable diagnostics on the container to send logs to Log Analytics
+# # Need to pass settings via JSON files to prevent shell from incorrectly parsing JSON
+# $LOGS_SETTING_PATH = ".\analyticsLogsSetting.json"
+# $METRICS_SETTING_PATH = ".\analyticsMetricsSetting.json"
+#
+# $LogsSettingsJson = @"
+# [{
+#     "category": "ContainerInstanceLog",
+#     "categoryGroup": "Logs",
+#     "enabled": true
+# }]
+# "@
+# $MetricsSettingJson = @"
+# [{
+#     "category": "AllMetrics",
+#     "enabled": true
+# }]
+# "@
+#
+# Set-Content -Path $LOGS_SETTING_PATH -Value $LogsSettingsJson
+# Set-Content -Path $METRICS_SETTING_PATH -Value $MetricsSettingJson
+#
+# az monitor diagnostic-settings create `
+#   --name "ACILoggingSettings" `
+#   --resource-group $RESOURCE_GROUP `
+#   --resource $ACI_NAME `
+#   --resource-type "Microsoft.ContainerInstance/containerGroups" `
+#   --workspace $LOG_ANALYTICS_WORKSPACE_ID `
+#   --logs @$LOGS_SETTING_PATH `
+#   --metrics @$METRICS_SETTING_PATH
+#
+# # Assign the "Log Analytics Contributor" role to the ACI principal ID
+# az role assignment create `
+#   --assignee $ACI_PRINCIPAL_ID `
+#   --role "Log Analytics Contributor" `
+#   --scope "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$LOG_ANALYTICS_RESOURCE_GROUP/providers/Microsoft.OperationalInsights/workspaces/$LOG_ANALYTICS_WORKSPACE_NAME"
+
+# Step 12: Remove the auto-generated JSON config files after deployment
 Remove-Item -Path $LOGIC_APP_DEFINITION_PATH -Force
+# Remove-Item -Path $LOGS_SETTING_PATH -Force
+# Remove-Item -Path $METRICS_SETTING_PATH -Force
+
+# manually check logs from ACI (even if Logs Analytics is not configured)
+# az container logs --resource-group $RESOURCE_GROUP --name $ACI_NAME
